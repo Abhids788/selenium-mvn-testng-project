19f6bbf50efc3f00be3532b5f010b98032e55f53
Abhids788 <abhids788@gmail.com>
2025-12-05 22:17:38 +0530

    updated code

---
+++ src/test/java/test/InvalidOrderTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.PageObjects.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import testComponents.BaseTest;
+
+public class InvalidOrderTest extends BaseTest {
+
+    @Test
+    public void submitOrder_Invalid() throws Exception {
+
+        ProductCatalogue productCatalogue =landingPage.loginToApplication("abhids791@gmail.com", "Asd@12345");
+        boolean match=productCatalogue.addProductToCart("Invalid Product");
+        Assert.assertFalse(match);
+    }
+
+}
